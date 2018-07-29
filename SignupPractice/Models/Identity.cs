71717762ed3f4f050df89068c5b79b497e5f01c7
Hashir Shoaib <hashirshoaeb@github.com>
2018-07-29 17:58:44 +0500

    HS: I commented the linq querry, it is not working. also i changed the UI a bit. added a nuget package named: Microsoft.AspNet.Identity

--- SignupPractice/Models/Identity.cs
+++ SignupPractice/Models/Identity.cs
@@ -22,6 +22,7 @@ namespace SignupPractice.Models
 
         public bool identitycheck(string c_email, string c_password)
         {
+            //var myIdentity = from e in Identies where e.email == c_email select e;
             foreach(Identity i in Identies)
             {
                 if (i.email == c_email && i.password == c_password)
